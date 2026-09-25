0ce4bed1fcd6ae1f13b55572c0a2199c27b5cd2f
Divya <45150201+Divyavani99@users.noreply.github.com>
2026-09-25 10:42:30 +0530

    Update WebClientConfig.java

--- order-service/src/main/java/com/fooddelivery/order/config/WebClientConfig.java
+++ order-service/src/main/java/com/fooddelivery/order/config/WebClientConfig.java
@@ -1,8 +1,8 @@
+
 package com.fooddelivery.order.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -13,3 +13,4 @@ public class WebClientConfig {
         return WebClient.builder();
     }
 }
+
