d7a3f438ac8f9eefc21039bbb53173641da4dd7a
Divya <45150201+Divyavani99@users.noreply.github.com>
2026-09-25 09:53:16 +0530

    Update UserServiceApplication.java

--- user-service/src/main/java/com/fooddelivery/user/UserServiceApplication.java
+++ user-service/src/main/java/com/fooddelivery/user/UserServiceApplication.java
@@ -2,10 +2,10 @@ package com.fooddelivery.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class UserServiceApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
