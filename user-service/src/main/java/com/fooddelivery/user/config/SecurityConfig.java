f231dc6a92ebbb0210d5899a0212e88c628a3473
Divya <45150201+Divyavani99@users.noreply.github.com>
2026-09-25 09:57:32 +0530

    Update SecurityConfig.java

--- user-service/src/main/java/com/fooddelivery/user/config/SecurityConfig.java
+++ user-service/src/main/java/com/fooddelivery/user/config/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.fooddelivery.user.security.JwtAuthFilter;
+import com.fooddelivery.user.config.JwtAuthFilter;
 
 @Configuration
 public class SecurityConfig {
